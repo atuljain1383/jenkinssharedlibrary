65c8377df11038643d444d3b0ece598d4c1e1a06
Atul Jain <83856750+atuljain1383@users.noreply.github.com>
2021-08-28 16:48:51 +0530

    Update runcontainer.groovy

--- vars/runcontainer.groovy
+++ vars/runcontainer.groovy
@@ -1,6 +1,6 @@
 def call(Map params){
   withEnv(["${params.hostipwithuser}"]) {
-    sshagent(['MServer2']) {
+    sshagent(["${params.server}"]) {
       sh "docker run -d --name "${params.containername}" -p "${params.hostport}":"${params.containerport}" --network=Bridge "${params.imagename}""
     }
   }
