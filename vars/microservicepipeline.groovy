f3090e28d289ba4cc2e849a1b0ffd4ccffb8d377
Atul Jain <83856750+atuljain1383@users.noreply.github.com>
2021-08-28 12:58:20 +0530

    Update microservicepipeline.groovy

--- vars/microservicepipeline.groovy
+++ vars/microservicepipeline.groovy
@@ -1,1 +1,26 @@
-
+def call(Map pipelineParams) {
+  pipeline{
+      agent any
+      stages{
+          stage("Checkout"){
+              steps{
+                gitcheckout([branch: "master", giturl: "https://github.com/atuljain1383/apis.git"])
+              }
+          }
+          stage("Build Image"){             
+              steps{
+                  dir("/var/lib/jenkins/workspace/OrderManagemenService/services/ordermanagement") {
+                      script{
+                              builddockerimage([imagename: "atuljain1383/ordermanagement:v3"])
+                      }
+                  }                    
+              }
+          }
+          stage("Push Image"){
+              steps{
+                 pushdockerimage([username: "atuljain1383", password:"Ishika@2sep", imagename: "atuljain1383/ordermanagement:v3"])
+             }  
+          }        
+      }
+  }
+}
