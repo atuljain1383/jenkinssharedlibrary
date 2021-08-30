0c818323e7522ce7553ff91aad8ff0db11e9f5c5
Atul Jain <83856750+atuljain1383@users.noreply.github.com>
2021-08-30 11:47:27 +0530

    Update runcontainer.groovy

--- vars/runcontainer.groovy
+++ vars/runcontainer.groovy
@@ -1,5 +1,5 @@
 def call(Map params){  
         sshagent(['"${params.server}"']) {
-          sh "sudo docker -h StrictHostKeyChecking=no ubuntu@172.31.11.14 run -d -p 3000:3001 --network=Bridge --name ordermanagementcontainer atuljain1383/ordermanagement:v4"
+          sh "ssh -o StrictHostKeyChecking=no ubuntu@172.31.11.14 sudo docker run -d -p 3000:3001 --network=Bridge --name ordermanagementcontainer atuljain1383/ordermanagement:v4"
     }
 }
